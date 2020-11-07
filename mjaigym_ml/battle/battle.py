0fe4e3a7685e138932691943711888ad15b002b6
rick0000 <rickdev0000@gmail.com>
2020-11-07 13:24:52 +0900

    install mlflow

---
+++ mjaigym_ml/battle/battle.py
@@ -0,0 +1,10 @@
+
+class Battle(object):
+    def __init__(
+        self,
+        agent0,
+        agent1,
+        agent2,
+        agent3,
+    ):
+        pass
---
+++ mjaigym_ml/battle/battlerunner.py
@@ -0,0 +1,7 @@
+"""対戦を行いモデルの評価を行う。
+"""
+
+
+class BattleRunner():
+    def __init__(self):
+        pass
