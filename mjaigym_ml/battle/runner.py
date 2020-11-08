259d02922d88c556ee9198f6005de3946a04338d
rick0000 <rickdev0000@gmail.com>
2020-11-08 22:05:40 +0900

    update runner sample

--- mjaigym_ml/battle/runner.py
+++ mjaigym_ml/battle/runner.py
@@ -1,6 +1,7 @@
 import time
 import argparse
 import subprocess
+import datetime
 from shlex import quote
 from threading import Thread
 import threading
@@ -9,34 +10,54 @@ from mjaigym.tcp.server import Server
 
 
 def run_manue(room_name, player_name):
-    proc = subprocess.run([
-            f"docker build -t manue:dev manue/. && docker run --rm manue:dev /mjai/mjai/run_manue.sh '{quote(player_name)}' '{quote(room_name)}'"
-            ], 
-        shell=True,
-        stdout=subprocess.DEVNULL,
-        )
+    # proc = subprocess.run([
+    #         f"docker build -t manue:dev manue/. && docker run --rm manue:dev /mjai/mjai/run_manue.sh '{quote(player_name)}' '{quote(room_name)}'"
+    #         ], 
+    #     shell=True,
+    #     stdout=subprocess.DEVNULL,
+    #     )
+    subprocess.run(
+        ["docker", "build", "-t", "manue:dev", "manue/."], 
+        shell=False,
+        stdout=subprocess.DEVNULL)
+    proc = subprocess.run(
+        ["docker", "run", "--rm", "manue:dev", "/mjai/mjai/run_manue.sh", f"{quote(player_name)}", f"{quote(room_name)}"], 
+        shell=False,
+        stdout=subprocess.DEVNULL)
+
     return proc
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=48000)
-    parser.add_argument("--clientlimit", type=int, default=32)
-    parser.add_argument("--logdir", type=str, default="./tcplog")
+    parser.add_argument("--clientlimit", type=int, default=128)
+    parser.add_argument("--logdir", type=str, default="/mnt/sdc/experiments/output/manue_log")
 
     args = parser.parse_args()
     server = Server(args.port, args.clientlimit, args.logdir)
     server.run_async()
 
-    room = "room0"
+    
 
-    threads = []
-    for i in range(4):
-        thread = threading.Thread(target=run_manue, args=(room, f"manue{i}"), daemon=True)
-        thread.start()
-        threads.append(thread)
+    
+    game_num = 1000
+    count = 0
+    per_games = 16
 
-    for t in threads:
-        t.join()
+    while game_count < game_num:
+        threads = []
+        count += per_games
+        for room_id in range(per_games):
+            print(datetime.datetime.now() , f"start game {room_id}")
+            for i in range(4):
+                thread = threading.Thread(target=run_manue, args=(f"room{room_id}", f"manue{i}"), daemon=True)
+                thread.start()
+                threads.append(thread)
+
+        for t in threads:
+            t.join()
+
+        
     
     # wait for server mjson log output
     time.sleep(5)
