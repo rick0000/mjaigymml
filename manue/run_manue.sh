350b4af0671dcfcc76530ae1803de2efefbea901
rick0000 <rickdev0000@gmail.com>
2020-11-24 10:39:08 +0900

    Fix manue battle runner efficiently.

--- manue/run_manue.sh
+++ manue/run_manue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GAME_HOST=`cat /etc/hosts | awk 'END{print $1}' | sed -r -e 's/[0-9]+$/1/g'`
-mjai-manue --name ${1} mjsonp://${GAME_HOST}:48000/${2} 
+mjai-manue --name ${1} mjsonp://${GAME_HOST}:${2}/${3} 
--- mjaigym_ml/battle/runner.py
+++ mjaigym_ml/battle/runner.py
@@ -1,63 +1,73 @@
 import time
 import argparse
 import subprocess
-import datetime
 from shlex import quote
-from threading import Thread
-import threading
-
+from concurrent import futures
+from concurrent.futures import ThreadPoolExecutor
 from mjaigym.tcp.server import Server
 
 
-def run_manue(room_name, player_name):
-    # proc = subprocess.run([
-    #         f"docker build -t manue:dev manue/. && docker run --rm manue:dev /mjai/mjai/run_manue.sh '{quote(player_name)}' '{quote(room_name)}'"
-    #         ], 
-    #     shell=True,
-    #     stdout=subprocess.DEVNULL,
-    #     )
-    subprocess.run(
-        ["docker", "build", "-t", "manue:dev", "manue/."], 
-        shell=False,
-        stdout=subprocess.DEVNULL)
-    proc = subprocess.run(
-        ["docker", "run", "--rm", "manue:dev", "/mjai/mjai/run_manue.sh", f"{quote(player_name)}", f"{quote(room_name)}"], 
-        shell=False,
-        stdout=subprocess.DEVNULL)
+def prepare_manue():
+    build_proc = subprocess.Popen([
+        "docker", "build", "-t", "manue:dev", "manue/."
+    ],
+        # shell=True,
+        stdout=subprocess.DEVNULL,
+    )
+    while True:
+        if build_proc.poll() is not None:
+            break
+        time.sleep(0.1)
 
-    return proc
+
+def run_manue(port, player_name, room_name):
+    processes = []
+
+    for i in range(4):
+        p_name = f"{quote(player_name)}{i}"
+        proc = subprocess.Popen([
+            "docker run --rm manue:dev /mjai/mjai/run_manue.sh " +
+            f"'{p_name}' '{port}' '{quote(room_name)}'"
+        ],
+            shell=True,
+            stdout=subprocess.DEVNULL,
+        )
+        processes.append(proc)
+
+    for proc in processes:
+        proc.wait()
+    return None
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=48000)
-    parser.add_argument("--clientlimit", type=int, default=128)
-    parser.add_argument("--logdir", type=str, default="/mnt/sdc/experiments/output/manue_log")
+    parser.add_argument("--logdir", type=str,
+                        default="./experiments/output/manue_log")
 
     args = parser.parse_args()
-    server = Server(args.port, args.clientlimit, args.logdir)
+
+    prepare_manue()
+
+    server = Server(args.port, 128, args.logdir)
     server.run_async()
 
-    
-
-    
     game_num = 1000
     count = 0
-    per_games = 16
+    per_games = 4
 
-    while game_count < game_num:
-        threads = []
-        count += per_games
-        for room_id in range(per_games):
-            print(datetime.datetime.now() , f"start game {room_id}")
-            for i in range(4):
-                thread = threading.Thread(target=run_manue, args=(f"room{room_id}", f"manue{i}"), daemon=True)
-                thread.start()
-                threads.append(thread)
+    with ThreadPoolExecutor(max_workers=per_games) as executor:
+        future_list = []
+        for i in range(game_num):
 
-        for t in threads:
-            t.join()
-
-        
-    
+            future = executor.submit(
+                run_manue,
+                args.port,
+                "TestManue",
+                f"room{i}"
+            )
+            future_list.append(future)
+        futures.as_completed(fs=future_list)
+        executor.shutdown()
     # wait for server mjson log output
     time.sleep(5)
